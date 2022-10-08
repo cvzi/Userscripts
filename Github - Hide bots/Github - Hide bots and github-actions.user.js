857cc28b95919efdb9de687bf0f654049b22c600
cvzi <cuzi@openmail.cc>
2022-10-08 02:15:03 +0200

    Bugfix: `class="push"` changed to `classes="push"`

--- Github - Hide bots/Github - Hide bots and github-actions.user.js
+++ Github - Hide bots/Github - Hide bots and github-actions.user.js
@@ -3,7 +3,7 @@
 // @description  Minimizes pushs and commits from github actions and bots from github.com dashboard
 // @namespace    cuzi
 // @author       cuzi
-// @version      1.4
+// @version      1.5
 // @description  Hide bot's and github-actions' push from dashboard news
 // @copyright    2020, cuzi (https://openuserjs.org/users/cuzi)
 // @license      GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
@@ -42,7 +42,7 @@
 
   function hideBots () {
     const expandButton = document.querySelector('button.js-details-target:not(.Header-link)[aria-expanded="false"]')
-    document.querySelectorAll('#dashboard div.push:not(.shotBot)').forEach(function (div) {
+    document.querySelectorAll('#dashboard div.push:not(.shotBot),#dashboard div[classes*=push]:not(.shotBot)').forEach(function (div) {
       const label = div.querySelector('.body .d-flex .d-flex .Label')
       const isAppUrl = div.querySelector('.body .d-flex .d-flex a.Link--primary[href^="/apps/"]')
       if (isAppUrl || (label && label.textContent === 'bot')) {
