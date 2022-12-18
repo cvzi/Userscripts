9b3ae3b3c63cb4075600f67e011a86f592a6b67b
cvzi <cuzi@openmail.cc>
2022-12-18 23:26:26 +0100

    Add openuserjs.org Autologin

---
+++ others/openuserjs.org Autologin.user.js
@@ -0,0 +1,60 @@
+// ==UserScript==
+// @name        openuserjs.org Autologin
+// @description Login and redirect to last viewed page
+// @namespace   openuserjs.org
+// @license     MIT
+// @version     13
+// @match       https://openuserjs.org/*
+// @exclude     /^https?:\/\/openuserjs\.org\/auth\/.*$
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM.setValue
+// @grant       GM.getValue
+// @require     https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @icon        https://openuserjs.org/images/favicon180.png
+// @sandbox     JavaScript
+// ==/UserScript==
+
+/* global GM */
+
+(function () {
+  'use strict'
+
+  GM.getValue('username', false).then(function (username) {
+    if (username && !document.getElementById('clearAutoLogin')) {
+      const li = document.createElement('li')
+      li.setAttribute('id', 'clearAutoLogin')
+      li.innerHTML = '<a href="#forget" title="Forget autologin"><span class="visible-xs-inline">Forget autologin</span><i class="fa fa-chain-broken"></i></a>'
+      document.querySelector('.navbar-nav').insertBefore(li, document.querySelector('.navbar-nav li:last-child'))
+      li.addEventListener('click', async function () {
+        if (window.confirm('Forget autologin?')) {
+          await GM.setValue('username', false)
+          window.alert('Your autologin username has been deleted.')
+        }
+      })
+    }
+
+    if (document.title.indexOf('503 ') === 0) {
+      // We're busy right now. Try again later.
+    } else if (document.location.href.match(/^https?:\/\/openuserjs\.org\/login\/?$/)) {
+      if (username) {
+        document.getElementsByName('username')[0].value = username
+        document.getElementsByName('consent')[0].checked = true
+        document.getElementById('action').click()
+      } else {
+        const button = document.getElementById('action')
+        button.addEventListener('click', async function (ev) {
+          const inputs = document.getElementsByName('username')
+          for (let j = 0; j < inputs.length; j++) {
+            if (inputs[j].value) {
+              await GM.setValue('username', inputs[j].value)
+              return
+            }
+          }
+        })
+      }
+    } else if (username && document.getElementsByClassName('fa fa-sign-in') && document.getElementsByClassName('fa fa-sign-in')[0]) {
+      document.getElementsByClassName('fa fa-sign-in')[0].click()
+    }
+  })
+})()
