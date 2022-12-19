62479ef735d81483d8cfff7d23d306e3d7804dac
cvzi <cuzi@openmail.cc>
2022-12-19 12:41:39 +0100

    Add Reverse Image Search userscript

---
+++ Reverse Image Search/README.md
@@ -0,0 +1,15 @@
+Reverse Image Search
+====================
+
+Right click an image and select "Open image in new tab", in the new tab press:
+
+*  `Ctrl + G` for [![Google Images](https://www.google.com/s2/favicons?sz=16&domain=google.com)](https://images.google.com/) [Google Images](https://images.google.com/)
+*  `Ctrl + Q` for [![TinEye](https://www.google.com/s2/favicons?sz=16&domain=tineye.com)](https://tineye.com) [TinEye](https://tineye.com/)
+*  `Ctrl + Y` for [![Yandex Images](https://www.google.com/s2/favicons?sz=16&domain=yandex.com)](https://yandex.com/images/) [Yandex Images](https://yandex.com/images/)
+*  `Ctrl + X` for [![SauceNAO](https://www.google.com/s2/favicons?sz=16&domain=saucenao.com)](https://saucenao.com/) [SauceNAO - Reverse Image Search](https://saucenao.com/)
+*  `Ctrl + ,` for [![iqdb.org](https://www.google.com/s2/favicons?sz=16&domain=iqdb.org)](https://iqdb.org/) [iqdb.org - Multi-service image search](https://iqdb.org/)
+
+Or go into the Tampermonkey menu on any website, and select the search service to search for the largest image on the page.
+
+
+![Screenshot](screenshot.png)
---
+++ Reverse Image Search/ReverseImageSearch.user.js
@@ -0,0 +1,112 @@
+// ==UserScript==
+// @name         Reverse Image Search
+// @namespace    github.com/cvzi/
+// @version      1.0.0
+// @description  Search largest image on the page or current image on Google, TinEye, Yandex, SauceNAO and iqdb.org
+// @author       cuzi
+// @copyright    2022, cuzi (https://github.com/cvzi/)
+// @license      GPL-3.0-or-later
+// @match        ://*/*
+// @icon         https://raw.githubusercontent.com/hfg-gmuend/openmoji/master/color/72x72/1F5BC.png
+// @grant        GM_openInTab
+// @grant        GM_registerMenuCommand
+// ==/UserScript==
+
+/*
+    Reverse Image Search
+    Search largest image on the page or current image on Google, TinEye, Yandex, SauceNAO and iqdb.org
+    Copyright (C) 2022, cuzi (https://github.com/cvzi/)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/* globals GM_openInTab, GM_registerMenuCommand */
+
+(function () {
+  'use strict'
+
+  function onKeyDown (keyCode, cb) {
+    return function (ev) {
+      if (document.activeElement === document.body) {
+        if (ev.ctrlKey && ev.key === keyCode) {
+          ev.preventDefault()
+          cb(ev)
+        }
+      }
+    }
+  }
+
+  function getUrl () {
+    if (document && document.body && document.body.firstChild.tagName === 'IMG') {
+      return document.location.href
+    } else {
+      const biggestImage = Array.from(document.querySelectorAll('img[src^=http],img[srcset]')).filter(img => img.src.startsWith('http')).map(img => {
+        const dim = img.getBoundingClientRect()
+        return {
+          img,
+          size: dim.width + dim.height
+        }
+      }).sort(o => o.size).map(o => o.img.src).pop()
+      if (biggestImage) {
+        return biggestImage
+      }
+    }
+    return document.location.href
+  }
+
+  function google () {
+    /* Google reverse image search */
+    GM_openInTab('https://www.google.com/imghp?sbi=1#habibi=' + encodeURIComponent(getUrl()), { active: true })
+  }
+
+  function tinEye () {
+    /* TinEye reverse image search */
+    GM_openInTab('https://tineye.com/search?url=' + encodeURIComponent(getUrl()))
+  }
+  function yandex () {
+    /* Yandex reverse image search */
+    GM_openInTab('https://yandex.com/images/search?rpt=imageview&url=' + encodeURIComponent(getUrl()))
+  }
+  function sauceNAO () {
+    /* SauceNAO reverse image search */
+    GM_openInTab('https://saucenao.com/search.php?url=' + encodeURIComponent(getUrl()))
+  }
+  function iqdb () {
+    /* iqdb.org reverse image search */
+    GM_openInTab('http://iqdb.org/?url=' + encodeURIComponent(getUrl()))
+  }
+
+  GM_registerMenuCommand('Reverse Image search - Google', google, 'g')
+  GM_registerMenuCommand('Reverse Image search - TinEye', tinEye, 'q')
+  GM_registerMenuCommand('Reverse Image search - Yandex', yandex, 'y')
+  GM_registerMenuCommand('Reverse Image search - SauceNAO', sauceNAO, 'x')
+  GM_registerMenuCommand('Reverse Image search - iqdb.org', iqdb, ',')
+
+  if (document && document.body && document.body.firstChild.tagName === 'IMG') {
+    document.addEventListener('keydown', onKeyDown('g', google))
+    document.addEventListener('keydown', onKeyDown('q', tinEye))
+    document.addEventListener('keydown', onKeyDown('y', yandex))
+    document.addEventListener('keydown', onKeyDown('x', sauceNAO))
+    document.addEventListener('keydown', onKeyDown(',', iqdb))
+  } else if (document.location.href.startsWith('/imghp') !== -1 && document.location.hash.startsWith('#habibi=')) {
+    // Enter url into Google search form
+    window.setTimeout(() => {
+      document.querySelector('[role="button"]>img[src]').click()
+      window.setTimeout(() => {
+        document.querySelector('input[text=text]').value = decodeURIComponent(document.location.hash.substring(8))
+        document.querySelector('input[text=text]').nextElementSibling.click()
+      }, 500)
+    }, 500)
+  }
+})()
