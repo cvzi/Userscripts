2ea985e2b30436780ad207387916ee915f05bcb6
cvzi <cuzi@openmail.cc>
2024-02-09 21:58:20 +0100

    Slow down to prevent hitting ratelimit

--- others/openuserjs.org Autologin.user.js
+++ others/openuserjs.org Autologin.user.js
@@ -3,7 +3,7 @@
 // @description Login and redirect to last viewed page
 // @namespace   openuserjs.org
 // @license     MIT
-// @version     14.1
+// @version     14.2
 // @match       https://openuserjs.org/*
 // @exclude     /^https?:\/\/openuserjs\.org\/auth\/.*$
 // @grant       GM_setValue
@@ -11,15 +11,21 @@
 // @grant       GM.setValue
 // @grant       GM.getValue
 // @require     https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
-// @require     https://cdn.jsdelivr.net/gh/Joe12387/detectIncognito@v1.2.0/detectIncognito.min.js
+// @require     https://cdn.jsdelivr.net/gh/Joe12387/detectIncognito@1.3.0/dist/es5/detectIncognito.min.js
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=openuserjs.org
 // @sandbox     JavaScript
 // ==/UserScript==
 
 /* global GM, detectIncognito */
 
+/* jshint asi: true, esversion: 8 */
+
 (function () {
   'use strict'
+
+  function sleep (t) {
+    return new Promise(resolve => setTimeout(resolve, t))
+  }
 
   const main = () => {
     GM.getValue('username', false).then(function (username) {
@@ -42,7 +48,9 @@
         if (username) {
           document.getElementsByName('username')[0].value = username
           document.getElementsByName('consent')[0].checked = true
-          document.getElementById('action').click()
+          sleep(500).then(() => {
+            document.getElementById('action').click()
+          })
         } else {
           const button = document.getElementById('action')
           button.addEventListener('click', async function (ev) {
@@ -56,7 +64,9 @@
           })
         }
       } else if (username && document.getElementsByClassName('fa fa-sign-in') && document.getElementsByClassName('fa fa-sign-in')[0]) {
-        document.getElementsByClassName('fa fa-sign-in')[0].click()
+        sleep(500).then(() => {
+          document.getElementsByClassName('fa fa-sign-in')[0].click()
+        })
       }
     })
   }
